2241fb8814a07166ed7824e4c5ff63272fe77c74
vmwhoami <vmwhoami@gmail.com>
2025-03-27 23:00:35 +0200

    add images for reference

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,24 @@
 <template>
   <main>
     <Navbar/>
- 
+    <IntroMain />
+    <TrainingPrograms />
+    <AboutSection />
+    <GroupClass />
+    <TrainerPostSection />
+    <PersonalShop />
+    <ClientSay />
+    <PersonalApp />
+    <Footer />
 
     
   </main>
 </template>
 <script setup>
 import Navbar from '~/components/Navbar.vue';
+import IntroMain from '~/components/mainPage/IntroMain'
+ 
+import TrainingPrograms from '~/components/mainPage/TrainingPrograms';
+import AboutSection from '~/components/mainPage/AboutSection.vue';
 const { data } = await useFetch("/api/tickers/?limit=10");
 </script>
