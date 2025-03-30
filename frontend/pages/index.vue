6afa4287ab28cf116c7e825c2a9a016a6610d0c0
vmwhoami <vmwhoami@gmail.com>
2025-03-30 18:50:03 +0300

    remove unecessary component

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -17,7 +17,6 @@ import Navbar from "@/components/Navbar.vue";
 import IntroMain from "@/components/mainPage/IntroMain";
 import TrainingPrograms from "@/components/mainPage/TrainingPrograms";
 import AboutSection from "@/components/mainPage/AboutSection.vue";
-import GroupClass from "@/components/mainPage/AboutSection.vue";
 import TrainerPostSection from "@/components/mainPage/TrainerPostSection.vue";
 import PersonalShop from "@/components/mainPage/PersonalShop.vue";
 import Testimonials from "@/components/mainPage/Testimonials.vue";
