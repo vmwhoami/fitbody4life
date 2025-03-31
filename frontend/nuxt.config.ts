9e9fa954950bfe9b5d39e3467cfad3c7abd00d13
vmwhoami <vmwhoami@gmail.com>
2025-03-31 21:38:07 +0300

    remove extra tag

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['@/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
