9227346710d4d7602a7c43276d260d67b8ec0c0d
vmwhoami <vmwhoami@gmail.com>
2025-04-03 20:54:41 +0300

    add localization

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,8 +4,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
-  modules: ['motion-v/nuxt'],
+  modules: ['motion-v/nuxt', '@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+      { code: 'ro', language: 'ro-RO' }
+    ],
+    defaultLocale: 'en',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
+ 
 })
