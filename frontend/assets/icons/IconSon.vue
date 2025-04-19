606607639823a6356fd3de32ee98693abb44d6eb
vmwhoami <vmwhoami@gmail.com>
2025-04-19 19:30:28 +0300

    organise a more readablele code in the nightdayswitcher

---
+++ frontend/assets/icons/IconSon.vue
@@ -0,0 +1,44 @@
+<template>
+  <IconBase
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    :iconColor="iconColor"
+    :fill="myFill"
+  >
+    <circle cx="14" cy="14" r="3.5" stroke="currentColor"></circle>
+    <path d="M14 8.5V6.5" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M17.889 10.1115L19.3032 8.69727" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M19.5 14L21.5 14" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M17.889 17.8885L19.3032 19.3027" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M14 21.5V19.5" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M8.69663 19.3029L10.1108 17.8887" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M6.5 14L8.5 14" stroke="currentColor" stroke-linecap="round"></path>
+    <path d="M8.69663 8.69711L10.1108 10.1113" stroke="currentColor" stroke-linecap="round"></path>
+  </IconBase>
+</template>
+
+<script setup>
+  import IconBase from './IconBase.vue'
+  const props = defineProps({
+    width: {
+      type: [Number, String],
+      default: 115,
+    },
+    height: {
+      type: [Number, String],
+      default: 40,
+    },
+    iconColor: {
+      type: String,
+      default: '##e3e3e3',
+    },
+    myFill: {
+      type: [Number, String],
+      default: '#e3e3e3',
+    },
+    viewBox: {
+      default: '0 0 28 28',
+    },
+  })
+</script>
--- frontend/components/DarkWhiteSwitche.vue
+++ frontend/components/DarkWhiteSwitche.vue
@@ -1,79 +1,44 @@
 <template>
-  <div
-    class="z-10 inline-grid grid-cols-2 gap-0.5 rounded-full bg-gray-950/5 p-0.75 text-gray-950 dark:bg-white/10 dark:text-white"
+  <div class="z-10 inline-grid grid-cols-2 gap-0.5 
+          rounded-full bg-gray-950/5 p-0.75 
+          text-gray-950 dark:bg-white/10 
+          dark:text-white"
     id="headlessui-radiogroup-«R65elb»"
-    role="radiogroup"
-  >
-    <span
-      class="rounded-full p-1.5 *:size-7 data-checked:bg-white data-checked:ring data-checked:inset-ring data-checked:ring-gray-950/10 data-checked:inset-ring-white/10 sm:p-0 dark:data-checked:bg-gray-700 dark:data-checked:text-white dark:data-checked:ring-transparent"
+    role="radiogroup">
+    
+    <span class="rounded-full p-1.5 *:size-7 
+                 data-checked:bg-white data-checked:ring
+                 data-checked:inset-ring data-checked:ring-gray-950/10
+                 data-checked:inset-ring-white/10 sm:p-0
+                 dark:data-checked:bg-gray-700 dark:data-checked:text-white
+                  dark:data-checked:ring-transparent"
       aria-label="Light theme"
       id="headlessui-radio-«Ram5elb»"
       role="radio"
       aria-checked="true"
       tabindex="0"
       data-headlessui-state="checked"
-      data-checked=""
-      ><svg viewBox="0 0 28 28" fill="none">
-        <circle cx="14" cy="14" r="3.5" stroke="currentColor"></circle>
-        <path d="M14 8.5V6.5" stroke="currentColor" stroke-linecap="round"></path>
-        <path
-          d="M17.889 10.1115L19.3032 8.69727"
-          stroke="currentColor"
-          stroke-linecap="round"
-        ></path>
-        <path d="M19.5 14L21.5 14" stroke="currentColor" stroke-linecap="round"></path>
-        <path
-          d="M17.889 17.8885L19.3032 19.3027"
-          stroke="currentColor"
-          stroke-linecap="round"
-        ></path>
-        <path d="M14 21.5V19.5" stroke="currentColor" stroke-linecap="round"></path>
-        <path
-          d="M8.69663 19.3029L10.1108 17.8887"
-          stroke="currentColor"
-          stroke-linecap="round"
-        ></path>
-        <path d="M6.5 14L8.5 14" stroke="currentColor" stroke-linecap="round"></path>
-        <path
-          d="M8.69663 8.69711L10.1108 10.1113"
-          stroke="currentColor"
-          stroke-linecap="round"
-        ></path></svg>
-        </span>
-        <span
-      class="rounded-full p-1.5 *:size-7 data-checked:bg-white data-checked:ring data-checked:inset-ring data-checked:ring-gray-950/10 data-checked:inset-ring-white/10 sm:p-0 dark:data-checked:bg-gray-700 dark:data-checked:text-white dark:data-checked:ring-transparent"
+      data-checked="">
+        <IconSon />
+    </span>
+    <span class="rounded-full p-1.5 *:size-7 data-checked:bg-white
+                 data-checked:ring data-checked:inset-ring 
+                 data-checked:ring-gray-950/10 data-checked:inset-ring-white/10
+                 sm:p-0 dark:data-checked:bg-gray-700 
+                 dark:data-checked:text-white 
+                 dark:data-checked:ring-transparent"
       aria-label="Dark theme"
       id="headlessui-radio-«Rem5elb»"
       role="radio"
       aria-checked="false"
       tabindex="-1"
-      data-headlessui-state=""
-      >
-      <svg viewBox="0 0 28 28" fill="none">
-        <path
-          d="M10.5 9.99914C10.5 14.1413 13.8579 17.4991 18 17.4991C19.0332 17.4991 20.0176 17.2902 20.9132 16.9123C19.7761 19.6075 17.109 21.4991 14 21.4991C9.85786 21.4991 6.5 18.1413 6.5 13.9991C6.5 10.8902 8.39167 8.22304 11.0868 7.08594C10.7089 7.98159 10.5 8.96597 10.5 9.99914Z"
-          stroke="currentColor"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M16.3561 6.50754L16.5 5.5L16.6439 6.50754C16.7068 6.94752 17.0525 7.29321 17.4925 7.35607L18.5 7.5L17.4925 7.64393C17.0525 7.70679 16.7068 8.05248 16.6439 8.49246L16.5 9.5L16.3561 8.49246C16.2932 8.05248 15.9475 7.70679 15.5075 7.64393L14.5 7.5L15.5075 7.35607C15.9475 7.29321 16.2932 6.94752 16.3561 6.50754Z"
-          fill="currentColor"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M20.3561 11.5075L20.5 10.5L20.6439 11.5075C20.7068 11.9475 21.0525 12.2932 21.4925 12.3561L22.5 12.5L21.4925 12.6439C21.0525 12.7068 20.7068 13.0525 20.6439 13.4925L20.5 14.5L20.3561 13.4925C20.2932 13.0525 19.9475 12.7068 19.5075 12.6439L18.5 12.5L19.5075 12.3561C19.9475 12.2932 20.2932 11.9475 20.3561 11.5075Z"
-          fill="currentColor"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-      </svg>
+      data-headlessui-state="">
+        <IconMoon />
     </span>
   </div>
 </template>
 
 <script setup>
-
+  import IconSon from '~/assets/icons/IconSon.vue'
+  import IconMoon from '~/assets/icons/IconMoon.vue'
 </script>
