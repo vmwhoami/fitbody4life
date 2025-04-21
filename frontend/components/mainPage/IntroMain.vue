62021501e35555ef47c5404f6c9a638f5eb9acc1
vmwhoami <vmwhoami@gmail.com>
2025-04-21 16:35:34 +0300

    add things to fix in next pr

--- frontend/components/mainPage/IntroMain.vue
+++ frontend/components/mainPage/IntroMain.vue
@@ -12,20 +12,20 @@
           been a Fitness Trainer since 2020, Fitness, Health mother and wife.
         </p>
         <div class="flex space-x-4">
-          <button class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">
+          <!-- <button class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">
             Get it on Google Play
           </button>
           <button class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">
             Get it on App Store
-          </button>
+          </button> -->
         </div>
       </div>
 
       <!-- Placeholder Image Column -->
+ 
       <div class="md:w-1/2 flex justify-center mb-8 md:mb-0">
-        <div
-          class="w-72 h-72 bg-gray-200 flex items-center justify-center rounded shadow-md"
-        >
+        <div class="w-72 h-72 bg-gray-200 flex items-center 
+                    justify-center rounded shadow-md"        >
           <span class="text-gray-400"><img src="@/assets/images/oleaup.jpeg" alt="olea"></span>
         </div>
       </div>
@@ -33,4 +33,6 @@
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+//  import { useMotionValue } from "motion-v"
+</script>
