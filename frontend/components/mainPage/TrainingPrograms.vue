af05e59ccb9db1acb117ddf3ec78b9a65142dbfc
vmwhoami <vmwhoami@gmail.com>
2025-07-04 13:56:46 +0300

    add icons

--- frontend/components/mainPage/TrainingPrograms.vue
+++ frontend/components/mainPage/TrainingPrograms.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="bg-gray-50 py-12">
     <div class="container mx-auto px-6">
-      <h2 class="text-2xl md:text-3xl font-bold mb-4">
-        Training Programs we offer for you.
-      </h2>
+      <h2 class="text-2xl md:text-3xl font-bold mb-4">Training Programs we offer for you.</h2>
       <p class="text-gray-600 mb-8">
-        Starting a fitness routine can be intimidating, but it helps to have an idea of
-        what to expect before joining any class.
+        Starting a fitness routine can be intimidating, but it helps to have an idea of what to
+        expect before joining any class.
       </p>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <!-- Card 1 -->
-        <div class="bg-white p-6 rounded shadow hover:shadow-lg transition-shadow">
+        <div
+          class="flex justify-center items-center bg-white p-6 rounded shadow hover:shadow-lg transition-shadow"
+        >
           <div class="mb-4">
-            <IconBackLift />
+            <IconPrivateLesson />
           </div>
           <h3 class="text-xl font-semibold mb-2">Private Session</h3>
           <p class="text-gray-600">
@@ -22,38 +22,25 @@
         </div>
 
         <!-- Card 2 -->
-        <div class="bg-white p-6 rounded shadow hover:shadow-lg transition-shadow">
+        <div
+          class="flex justify-center items-center bg-white p-6 rounded shadow hover:shadow-lg transition-shadow"
+        >
           <div class="mb-4">
-            <svg
-              class="w-40 h-40 text-orange-500"
-              fill="currentColor"
-              viewBox="0 0 200 200">
-              <path d="M10 2a6 6 0 00-6 6v1a4 4 0 001.528 3.172 2 2 0 01.54 1.76l-.436
-                       3.058A1 1 0 006.618 18h6.764a1 1 0 00.986-.83l.436-3.058a2 2 0 
-                       01.54-1.76A4 4 0 0016 9V8a6 6 0 00-6-6z"/>
-            </svg>
+            <IconDumbelPose />
           </div>
           <h3 class="text-xl font-semibold mb-2">Group Class</h3>
-          <p class="text-gray-600">
-            We offer an array of different group fitness class types.
-          </p>
+          <p class="text-gray-600">We offer an array of different group fitness class types.</p>
         </div>
 
         <!-- Card 3 -->
-        <div class="bg-white p-6 rounded shadow hover:shadow-lg transition-shadow">
+        <div
+          class="flex justify-center items-center bg-white p-6 rounded shadow hover:shadow-lg transition-shadow"
+        >
           <div class="mb-4">
-            <svg class="w-40 h-40 text-orange-500"
-                 fill="currentColor"
-                 viewBox="0 0 200 200">
-              <path d="M4 3a2 2 0 00-2 2v3a4 4 0 004 4h2a2 2 0 110 4H5a1 1 0 100 
-                       2h3a4 4 0 000-8H8a2 2 0 01-2-2V5a2 2 0 114 0v2a1 1 0 102 
-                       0V5a4 4 0 00-4-4H4z"/>
-            </svg>
+            <IconProgramDiet />
           </div>
           <h3 class="text-xl font-semibold mb-2">Program & Diet</h3>
-          <p class="text-gray-600">
-            You need to begin thinking of the amount of protein.
-          </p>
+          <p class="text-gray-600">You need to begin thinking of the amount of protein.</p>
         </div>
       </div>
     </div>
@@ -61,6 +48,9 @@
 </template>
 
 <script setup>
- import IconBackLift from 'assets/icons/IconBackLift.vue'
+  //TrainingPrograms
 
+  import IconPrivateLesson from '~/assets/icons/IconPrivateLesson.vue'
+  import IconProgramDiet from '~/assets/icons/IconProgramDiet.vue'
+  import IconDumbelPose from '~/assets/icons/IconDumbelPose.vue'
 </script>
