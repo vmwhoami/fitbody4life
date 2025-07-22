6a5c3c75058e6dbcbda8f8fb7f4488c5acde978a
vmwhoami <vmwhoami@gmail.com>
2025-07-22 19:49:07 +0300

    change messaging

--- frontend/app/components/Footer.vue
+++ frontend/app/components/Footer.vue
@@ -3,7 +3,14 @@
     <div class="container mx-auto px-6 text-center">
       <ul class="flex justify-center space-x-4 mb-4 text-gray-600 text-sm md:text-base">
         <li><NuxtLink to="/" class="hover:text-gray-900">Home Page</NuxtLink></li>
-      
+      <li>
+        <NuxtLink href="https://vmwhoami.github.io/fitbody4life-policy/privacy.html"
+                  target="_blank"
+                  rel="noopener"
+                  class="hover:text-gray-900">
+            Privacy Policy
+        </NuxtLink>
+      </li>
       </ul>
       <p class="text-gray-500 text-sm">
         &copy;vmwhoami {{ currentYerar }}. All rights reserved.
