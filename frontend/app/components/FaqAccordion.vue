b1611a586a9d2c7b032522c8d2fc191d9389376f
vmwhoami <vmwhoami@gmail.com>
2025-07-31 15:57:52 +0300

    add arrow svgs

--- frontend/app/components/FaqAccordion.vue
+++ frontend/app/components/FaqAccordion.vue
@@ -15,18 +15,7 @@
                 :aria-controls="`faq-body-${idx}`"
                 @click="toggle(idx)">
                 {{ item.question }}
-          <svg
-            class="w-6 h-6 shrink-0 transition-transform"
-            :class="{ 'rotate-180': activeIndex === idx }"
-            fill="currentColor"
-            viewBox="0 0 20 20">
-            <!-- Full chevron path -->
-            <path fill-rule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.936a.75.75 
-                 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 
-                 0 01.02-1.06z"
-              clip-rule="evenodd"/>
-          </svg>
+           <IconArrow  :class="{ 'rotate-180': activeIndex === idx }"/>
         </button>
       </h2>
       <div :id="`faq-body-${idx}`"
@@ -41,7 +30,7 @@
 
 <script setup>
 import { ref } from 'vue';
-
+import IconArrow from '~/assets/icons/IconArrow.vue';
 const props = defineProps({
   faqs: {
     type: Array,
